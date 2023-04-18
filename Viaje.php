4f6618b24aef3cbb11734f15accae449ca84c6dd
Cocacota <julian.tolosa@est.fi.uncoma.edu.ar>
2023-04-18 15:17:56 -0300

    entrega 2

--- Viaje.php
+++ Viaje.php
@@ -6,11 +6,13 @@ class viaje{
     private  $destino;
     private  $maxPasajeros;
     private  $pasajeros;
-    public function __construct(){ 
+    private $responsable;
+    public function __construct(){
         $this->codigo=0 ;
         $this->destino="";
         $this->maxPasajeros=0;
         $this->pasajeros=[];
+        $this->responsable=null;
     }
     public function getCodigo(){
         return $this->codigo;
@@ -33,8 +35,13 @@ class viaje{
     public function setMaxPasajeros($maxPasajeros){
         $this->maxPasajeros=$maxPasajeros;
     }
+    public function getResponsable(){
+        return $this->responsable;
+    }
+    public function setResponsable($respo){
+        $this->responsable=$respo;
+    }
     public function modificarPasajero($p){
-<<<<<<< HEAD:Viaje.php
         //$p array
         $cambio=false;
         $mensaje="pasajero no encontrado \n";
@@ -44,17 +51,6 @@ class viaje{
                 $this->pasajeros[$i]=$p;
                 $cambio=true;
                 $mensaje="pasajero modificado\n";
-=======
-        //$p array con los datos del pasajero a modificar
-        $cambio=true;
-        $mensaje="pasajero no encontrado \n";
-        $i=0;
-        do{
-            if($p["documento"]==$this->getPasajeros()[$i]["documento"]){ //se busca al pasajero por el documento 
-                $this->pasajeros[$i]=$p;
-                $cambio=false;
-                $mensaje="pasajero modificado\n";//la bandera para que termine de recorrer el arreglo una ves encontrado el pasajero
->>>>>>> 7b74eabd411708a0c90fa8931beec79d498176e7:tpEntregable1.php
             }
             $i++;
         }while($i<count($this->getPasajeros())&&!$cambio);
@@ -62,7 +58,6 @@ class viaje{
         return $mensaje;
     }
     public function cargarPasajero($p){
-<<<<<<< HEAD:Viaje.php
             //$p objeto 
             $mensaje="";
             $i=0;
@@ -77,12 +72,6 @@ class viaje{
             }
             if(!$encontro){
             $this->getpasajeros()[]=$p;
-=======
-            //$p arreglo con los datos del pasajero a cargar
-            $mensaje="";
-        if(count( $this->getPasajeros())<$this->getMaxPasajeros()){ //se comprueva si todabia queda lugar para mas pasajeros
-            $this->pasajeros[count( $this->getPasajeros())]=$p;
->>>>>>> 7b74eabd411708a0c90fa8931beec79d498176e7:tpEntregable1.php
             $mensaje="pasajero cargado \n";
             }
         }else{
@@ -91,16 +80,10 @@ class viaje{
         return $mensaje;
     }
     public function __toString(){
-<<<<<<< HEAD:Viaje.php
         $mensaje="pasajeros del viaje \n";
         for($i=0;$i<count($this->getPasajeros());$i++){
             $mensaje= $mensaje . $this->getPasajeros()[$i] ."\n";
-=======
-        $mensaje="pasajeros del viaje: \n";
-        for($i=0;$i<count($this->getPasajeros());$i++){//genero un mensaje con los datos de todos los pasajeros
-            $mensaje= $mensaje . "documento: ". $this->getPasajeros()[$i]["documento"]." nombre: ". $this->getPasajeros()[$i]["nombre"]. " apellido: ". $this->getPasajeros()[$i]["apellido"]."\n";
->>>>>>> 7b74eabd411708a0c90fa8931beec79d498176e7:tpEntregable1.php
         }
-        return "codigo del viaje ". $this->getCodigo(). " destino: ". $this->getDestino(). " pasajeros Maximos ". $this->getMaxPasajeros()."\n". $mensaje."\n";
+        return "codigo del viaje ". $this->getCodigo(). " destino: ". $this->getDestino(). " pasajeros Maximos ". $this->getMaxPasajeros(). $mensaje."\n";
     }
 }
